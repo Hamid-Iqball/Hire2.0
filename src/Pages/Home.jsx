5a18932b167471f361c09d8a55af891578cba356
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-01-01 17:52:02 +0500

    design the job description component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
           <span className="mb-4">
             <h2 className="text-lg font-semibold">Available Vacancies at Veevo Tech Official
             </h2>
-            {/* <p className="text-sm">In-case of any issues, please email us at HR@veevotech.com</p> */}
+          
           </span>
 
           <div
@@ -39,24 +39,34 @@ function Home() {
             <p className="font-[500]">AI/ML R&D Engineer</p>
           </div>
 
-          
+          <div
+            onClick={() => navigate("/vacancy/ai-ml-rd-engineer")}
+            className="flex justify-start items-center gap-2 rounded-xl p-2 max-w-[85%] mb-2 cursor-pointer bg-white hover:bg-[#57A8FF] hover:text-white active:bg-[#57A8FF] active:text-white duration-300 ease-in-out group"
+          >
+            <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300 ease-in-out">
+              <FaRegCheckCircle size={20} />
+            </span>
+            <p className="font-[500]">AI/ML R&D Engineer</p>
+          </div>
+
 
         </motion.div>
       </div>
 
-      <div className="flex justify-center items-center overflow-y-hidden relative">
+      <div className="flex justify-center items-center  relative">
         {/* The Hero Image */}
         <motion.img
           initial={{ y: 200, opacity: 0 }}
           animate={{ y: 0, opacity: 1 }}
           transition={{ duration: 1, ease: "easeInOut" }}
-          src="Images/Group 707.png"
+          src="/Images/Group 707.png"
           alt="Hero Image"
           className="absolute "
         />
         
         {/* Outlet for Job Details */}
-        <div className="absolute inset-12">
+        <div 
+         className="absolute inset-8 mb-4 h-auto">
           <Outlet />
         </div>
 
--- src/Pages/JobDetails.jsx
+++ src/Pages/JobDetails.jsx
@@ -1,17 +1,146 @@
+
+
 import { Button } from "@material-tailwind/react";
+import { BsGenderAmbiguous } from "react-icons/bs";
+import { FaCalendar } from "react-icons/fa";
+import { FaPeopleGroup } from "react-icons/fa6";
+import { GiGraduateCap } from "react-icons/gi";
+import { GrDocumentText } from "react-icons/gr";
+import { LuMapPinHouse } from "react-icons/lu";
+import { MdDateRange } from "react-icons/md";
+import { PiOfficeChairFill } from "react-icons/pi";
+import { TiArrowBack } from "react-icons/ti";
 import { useNavigate, useParams } from "react-router-dom";
 
 function JobDetails() {
 const { id } = useParams();
 const navigate = useNavigate()
   return (
-    <div className="bg-white p-6 rounded-lg shadow-lg">
-      <h2 className="text-xl font-semibold mb-4">{id}</h2>
-      <div className="flex flex-col justify-start items-center">
 
+    <div className="bg-white p-6 px-8 rounded-2xl border-[8px] border-gray-900  ">
+      <h2 className="text-xl font-semibold mb-4 flex justify-between items-center">{id} <span  onClick={()=>navigate(-1)} className="cursor-pointer"><TiArrowBack color="#57A8FF" size='24px'/></span></h2>
+      <div>
+
+        {/* Section - 1 */}
+        <div className="flex justify-between items-center gap-1 border-b-2 border-dashed px-4 py-6 ">
+
+        <div className="grid grid-cols-[0.9fr,1fr] gap-3">
+            <div className="border border-blue-400 p-1 rounded-full flex justify-center items-center">
+            <BsGenderAmbiguous size='26px' color="#57A8FF"/>
+            </div>
+            <div className="text-sm">
+                <p>Gender</p>
+                <strong>Both</strong>
+            </div>
+        </div>
+
+        <div className="grid grid-cols-[0.7fr,1fr] gap-3">
+        <div className="border border-blue-400  rounded-full flex justify-center items-center">
+        <FaPeopleGroup size='26px' color="#57A8FF"/>
+        </div>
+        <div className="text-sm">
+            <p>Age limit</p>
+            <strong>25 - 30</strong>
+        </div>
+        </div>
+
+        <div className="grid grid-cols-[0.6fr,1fr] gap-3">
+        <div className="border border-blue-400  rounded-full flex justify-center items-center">
+        <FaPeopleGroup size='26px' color="#57A8FF"/>
+        </div>
+        <div className="text-sm">
+            <p>Jon Type</p>
+            <strong>Office job</strong>
+        </div>
+        </div>
+  
+        </div>
+
+        {/* Section - 2 */}
+        <div className="flex justify-between items-center gap-1 border-b-2 border-dashed px-4 py-6">
+
+        <div className="grid grid-cols-[0.6fr,1fr] gap-3">
+            <div className="border border-blue-400 p-1 rounded-full flex justify-center items-center">
+            <PiOfficeChairFill size='26px' color="#57A8FF"/>
+            </div>
+            <div className="text-sm">
+                <p >Total Seats</p>
+                <strong>5</strong>
+            </div>
+        </div>
+
+        <div className="grid grid-cols-[0.4fr,1fr] ml-2 gap-2">
+        <div className="border border-blue-400  rounded-full flex justify-center items-center">
+        <MdDateRange size='26px' color="#57A8FF"/>
+        </div>
+        <div className="text-sm">
+            <p >Deadline Date</p>
+            <strong>Undisclosed</strong>
+        </div>
+        </div>
+
+        <div className="grid grid-cols-[0.5fr,1fr] gap-2">
+        <div className="border border-blue-400  rounded-full flex justify-center items-center">
+        <FaCalendar size='26px' color="#57A8FF"/>
+        </div>
+        <div className="text-sm">
+            <p>Experience</p>
+            <strong>0 year(s)</strong>
+        </div>
+        </div>
+
+        </div>
+
+
+        {/* Section - 3 */}
+        <div className="grid grid-cols-2 px-4 py-6 border-b-2 border-dashed">
+
+        <div className="grid grid-cols-[0.2fr,1fr] gap-2">
+            <div className="border border-blue-400 p-1 rounded-full flex justify-center items-center">
+            <LuMapPinHouse size='26px' color="#57A8FF"/>
+            </div>
+            <div className="text-sm">
+                <p>Location</p>
+                <strong>Peshawar, Islamabad</strong>
+            </div>
+        </div>
+
+            <div className="grid grid-cols-[0.2fr,1fr] ml-2 gap-2">
+            <div className="border border-blue-400  rounded-full flex justify-center items-center">
+            <GiGraduateCap size='26px' color="#57A8FF"/>
+            </div>
+            <div className="text-sm">
+                <p className="text-sm">Required Eduaction</p>
+                <strong>Any degree</strong>
+            </div>
+            </div>
+
+     
+
+        </div>
+
+
+        {/*Description  */}
+        <div className="grid grid-cols-1 px-4 py-6 border-b-2 border-dashed">
+        <div className="grid grid-cols-[0.1fr,1fr] gap-2">
+
+        <div className="border border-blue-400  rounded-full flex justify-center items-center p-2 place-self-start">
+        <GrDocumentText size='26px' color="#57A8FF"/>
+        </div>
+        <div className="text-sm">
+            <strong>Description</strong>
+            <p>If you are dreaming to be a part of visionary young people, if you are eager to join VT for making lives more technological, more easier with your efforts. 
+            We respect your spirit, come join our journey! <br /> Our internships are 90% leading to permanent jobs.We are offering internships in multiple departments, including both technical & management/marketing.</p>
+        </div>
+        </div>
+        </div>
+
+
+         {/* Last div */}
+         <div className=" px-4 py-6 border-b-2">
+            <Button className="bg-[#57A8FF]">Login to Update</Button>
+         </div>
       </div>
-
-      <Button onClick={()=>navigate(-1)}>Back</Button>
     </div>
   );
 }
