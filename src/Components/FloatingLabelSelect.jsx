3fa92bf4f014d2d1c3874aba85cfd08e8309ec00
Hamid-Iqball <hamid.iqbal00123@gmail.com>
2025-01-07 16:54:19 +0500

    completed the design of form component in PersonalInfo Page

---
+++ src/Components/FloatingLabelSelect.jsx
@@ -0,0 +1,61 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import Select from "react-select";
+
+const customStyles = {
+  control: (provided, state) => ({
+    ...provided,
+    backgroundColor: "",
+    borderColor: state.isFocused ? "#57A8FF" : "#B0BEC5", // Match hover color on focus
+    borderWidth: "1px",
+    borderRadius: "0.375rem",
+    boxShadow: "none",
+    "&:hover": {
+      borderColor: "#57A8FF",
+      borderWidth: "2px",
+    },
+  }),
+  placeholder: () => ({
+    display: "none",
+  }),
+  option: (provided, state) => ({
+    ...provided,
+    backgroundColor: state.isSelected
+      ? "#2563eb"
+      : state.isFocused
+      ? "#bfdbfe"
+      : "#fff",
+    color: state.isSelected ? "#fff" : "#000",
+  }),
+};
+
+function FloatingLabelSelect({ label, options }) {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isSelected, setIsSelected] = useState(false);
+
+  return (
+    <div className="relative">
+      <label
+        className={`absolute left-2 text-sm transition-all duration-200 ${
+          isFocused || isSelected
+            ? "-top-2 text-blue-500 text-xs mx-1 z-10 bg-[#F4F7FA]"
+            : "top-[9px] text-sm z-10 left-3 text-[#979ea1]"
+        } 
+        
+        ${!isFocused?"text-[#979ea1]": "text-blue-500"}
+        `}
+      >
+        {label}
+      </label>
+      <Select
+        options={options}
+        styles={customStyles}
+        onFocus={() => setIsFocused(true)} // Float the label on focus
+        onBlur={() => setIsFocused(false)} // Reset focus state
+        onChange={() => setIsSelected(true)} // Update selection state
+      />
+    </div>
+  );
+}
+
+export default FloatingLabelSelect;
--- src/Pages/PersonalInformation.jsx
+++ src/Pages/PersonalInformation.jsx
@@ -1,16 +1,55 @@
+import { Button, Input, Textarea } from "@material-tailwind/react";
+import FloatingLabelSelect from  "../Components/FloatingLabelSelect"
 
 
 function PersonalInformation() {
+  const genderOptions = [
+    { value: "Male", label: "Male" },
+    { value: "Female", label: "Female" },
+  ];
+
+  const maritalStatusOptions = [
+    { value: "Single", label: "Single" },
+    { value: "Married", label: "Married" },
+  ];
+
+  const countryOption =[
+    {value:"Pakistan", label:'Pakistan'},
+    {value:"Germany", label:"Germany" }
+  ]
+
+  const cityOption =[
+    {value:"Peshawar", label:"Peshawar"},
+    {value:"Islamabad", label:"Islamabad"}
+  ]
+
   return (
-    <section className="grid grid-cols-2 gap-4 mt-5">
-      <div>
-        form is header
-      </div>
+    <section className="grid grid-cols-2 gap-4 mt-8 ">
+    
+      <form className="grid grid-cols-2 gap-x-8 gap-y-0">
+        <Input label="Name" color="blue" />
+        <Input label="Father Name" color="blue" />
+        <Input label="Date-of-Birth" color="blue" />
+        <FloatingLabelSelect label="Gender" options={genderOptions} />
+        <FloatingLabelSelect label="Marital Status" options={maritalStatusOptions} />
+        <Input label="CNIC" color="blue" />
+        <FloatingLabelSelect label="Country" options={countryOption}/>
+        <FloatingLabelSelect label='City' options={cityOption}/>
+        <Input label="Contact #" color="blue"/>
+        <Input label="Email" color="blue"/>
+        <Textarea label="Current address" color="blue"/>
+        <Textarea label="Permanat address" color="blue"/>
+      </form>
+
       <div className="flex justify-center items-center">
         <img src="/Images/personal.png" alt="image" className="w-2/3" />
       </div>
+      <div className="flex justify-start col-span-full items-start gap-5">
+      <Button variant="outlined" size="sm" color="blue" >Update CV</Button>
+      <Button variant="outlined" size="sm" color="blue" >Update Profile</Button>
+      </div>
     </section>
-  )
+  );
 }
 
-export default PersonalInformation
+export default PersonalInformation;
